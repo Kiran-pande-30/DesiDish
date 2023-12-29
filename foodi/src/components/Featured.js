bc44cfe8666ebb6e676987a291d042ba49c91a30
Kiran-pande-30 <kpande026@gmail.com>
2023-12-29 23:45:55 +0530

    Slider-1

--- foodi/src/components/Featured.js
+++ foodi/src/components/Featured.js
@@ -1,22 +1,31 @@
 import React,{useState} from 'react'
+import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs'
 
 
 const Featured = () => {
     const sliders = [
+        // {
+        //     url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80'
+        // },
         {
-            url: 'https://drive.google.com/drive/u/1/home://images.app.goo.gl/9dTbSJFb6FmaskXK9'
-        },
-        {
-            url: 'https://images.app.goo.gl/8vJpjrXAFi82nEJg7'
-        },
+            url: 'https://images.unsplash.com/photo-1493770348161-369560ae357d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        }
     ]
 
     const [currentIndex, setCurrentIndex] = useState(0)
   return (
-    <div className='max-w-[1520] h-[500px] w-full py-4 px-4 relative'>
+    <div className='max-w-[1520] h-[500px] w-full py-4 px-4 relative group'>
         <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
             style={{backgroundImage: `url(${sliders[currentIndex].url})`}}
         ></div>
+
+        <div className=' hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+            <BsChevronCompactLeft/>
+        </div>
+
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+            <BsChevronCompactRight/>
+        </div>
     </div>
   )
 }
